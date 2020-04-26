c37428baad924723ebcb29113fd0ebbff37273f1
_Jacce_ <jacob.ogren2@gmail.com>
2020-04-26 19:41:36 +0200

    Update FileLogger.h
    Removed white space

--- Keylogger/FileLogger.h
+++ Keylogger/FileLogger.h
@@ -2,9 +2,9 @@
 #include <fstream>
 
 namespace FileLogger {
-	 void Log(const std::string& message) {
-			std::ofstream os("Log.txt", std::ios::app);
-			os << message;
-			os.close();
+	void Log(const std::string& message) {
+		std::ofstream os("Log.txt", std::ios::app);
+		os << message;
+		os.close();
 	}
 };
